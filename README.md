ad463f263d044e90902e612bfd82582f54a00f26
Juncheng-Li <Juncheng.alex.li@gmail.com>
2019-04-26 22:44:43 +1000

    readme updated

--- README.md
+++ README.md
@@ -4,28 +4,28 @@ The project applied three algorithms on the spelling corrction task, including E
 
 Each algorithm is written in a stand-alone python file.
 
-ED.py:
+**ED.py:**
 Predicts the correct word by calculating the Edit distance between token and words in the dictionary.
-
+```
 edit_distance(word1, word2): Calculates the edit_distance between two words
+```
 
-
-nGram.py:
+**nGram.py:**
 Predicts the correct word by splitting the token into substrings of two letters.
-
+```
 Splits(string, length, step=1) and grams(word, n): splits words into substrings of n letters, in this project n = 2.
 intersection(lst1, lst2): Counts how many grams in common
 calc(String1, String): Calculates the Bigram score
+```
 
-
-Soundex.py:
+**Soundex.py:**
 Converts word into Soundex code first, then apply Edit Distance to calculate the similarity between words.
-
+```
 soundex_distance(word1, word2): Converts words into Soundex code and then calculates the Soundex distance.
+```
 
-
-Each file has following functions:
-
+**Each file has following functions:**
+```
 data_loading:
 Loads data from misspell.txt, correct.txt, dict.txt and store them respectively in three lists.
 
@@ -37,10 +37,7 @@ Store word predictions as well as corresponding correct and misspell words in a 
 
 predict_evaluation:
 Calculates precision and recall of the prediction
-
-
-
-
+```
 
 
 
--- Soundex.py
+++ Soundex.py
@@ -65,26 +65,6 @@ def predict_evaluation(correct, best_matches):
     precision = tp / tp_n_fp
     tp_n_fn = len(best_matches)
     recall = tp / tp_n_fn
-    return precision, recall, tp, tp_n_fp, tp_n_fn
-
-
-def detect_evaluation(correct, misspell, best_matches):
-    tp = 0
-    fn = 0
-    tn = 0
-    fp = 0
-    for e in range(0, len(best_matches)):
-        if correct[e] == misspell[e] and misspell[e] in best_matches[e]:
-            tp += 1
-        if correct[e] != misspell[e] and misspell[e] not in best_matches[e]:
-            fn += 1
-        if correct[e] == misspell[e] and misspell[e] not in best_matches[e]:
-            tn += 1
-        if correct[e] != misspell[e] and misspell[e] in best_matches[e]:
-            fp += 1
-
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
     return precision, recall
 
 
@@ -97,6 +77,7 @@ for i in range(0, len(misspell)):
     score = []
     lis = []
     for d in dic:
+        # Calculates Soundex distance for each
         value = soundex_distance(misspell[i], d)
         score.append(value)
     best = min(score)
@@ -105,15 +86,15 @@ for i in range(0, len(misspell)):
         lis.append(dic[j])
     best_matches.append(lis)
 
-print(best_matches)
+# Calculates the prediction value
 pred_evaluation = predict_evaluation(correct, best_matches)
-det_evaluation = detect_evaluation(correct, misspell, best_matches)
-eva = []
-eva.append(pred_evaluation)
-eva.append(det_evaluation)
-write_corrected(correct, misspell, best_matches, eva)
-write_corrected_verbose(correct, misspell, best_matches, eva)
+print("Precision: " + str(pred_evaluation[0]))
+print("Recall: " + str(pred_evaluation[1]))
 
+# Write results to files
+write_corrected(correct, misspell, best_matches, pred_evaluation)
+write_corrected_verbose(correct, misspell, best_matches, pred_evaluation)
+
+# Prints total processing time of the program
 print(time.clock())
 
-# print(neighborhood_search('lended', 'commodity'))
